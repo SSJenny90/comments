f17674e01c8da55abef5e9212034ee8242c15bd5
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-02-07 18:02:08 +1030

    add floating form

--- comments/forms.py
+++ comments/forms.py
@@ -1,9 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 from django_comments_xtd.forms import XtdCommentForm
-from crispy_bootstrap5.bootstrap5 import FloatingField
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Field, Submit, Hidden
-from core.widgets import CustomCheckbox
+from crispy_forms.layout import Layout, Field, Submit
 from django.urls import reverse
 import django_comments
 from django import forms
@@ -34,4 +32,30 @@ class SuperCommentForm(XtdCommentForm):
             Field('comment', rows='5', placeholder='Your comment'),
             Field('followup', template='forms/checkbox.html'),
             Submit('post','send'),
+        )
+
+
+class FloatingCommentForm(XtdCommentForm):
+    next = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_id = 'floatingCommentForm'
+        self.helper.form_method = 'POST'
+        self.helper.form_action = django_comments.get_form_target()
+        self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+            Field('next', value=reverse('comments-xtd-sent') + '#commentTree', type="hidden"),
+            'content_type',
+            'object_pk',
+            'timestamp',
+            'security_hash',
+            'reply_to',
+            Field('name', type="hidden"),
+            Field('email', type="hidden"),
+            Field('url', type="hidden"),
+            Field('honeypot', type="hidden"),
+            Field('comment', rows='5', placeholder='Your comment'),
+            Field('followup', value='true', template='forms/checkbox.html', type="hidden"),
         )
